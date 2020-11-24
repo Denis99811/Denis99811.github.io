2f4c0dce834d7eed2ce90b40445db22c98769458
Denis99811 <48827513+Denis99811@users.noreply.github.com>
2020-11-24 18:46:37 +0400

    Add files via upload

---
+++ push-worker.js
@@ -0,0 +1,6 @@
+self[`appKey`] = `222dee2d38d3816d6196449611e8b5f8`;
+self[`hostUrl`] = `https://cdn.gravitec.net/sw`;
+self.importScripts(`${self[`hostUrl`]}/worker.js`);
+// uncomment and set path to your service worker
+// if you have one with precaching functionality (has oninstall, onactivate event listeners)
+// self.importScripts('path-to-your-sw-with-precaching')
